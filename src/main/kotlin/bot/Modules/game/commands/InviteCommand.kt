211b7a42f9c22d127c1a224a34c2f42cb4b7b32b
unknown <ashkarmalek0@gmail.com>
2021-07-20 22:48:37 -0400

    :sparkles: Max amount of 4 people per party

--- src/main/kotlin/bot/Modules/game/commands/InviteCommand.kt
+++ src/main/kotlin/bot/Modules/game/commands/InviteCommand.kt
@@ -24,54 +24,62 @@ class InviteCommand: Command() {
             if(playerData.uuid != null) {
                 val party = Bot.database.partyRepository.findPartyWithPlayer(Player(playerData.id!!, playerData.uuid))
                 if(party != null && party.leaderId == playerData.id) {
-                    val pastInvite = Bot.database.partyInviteRepository.findInvite(context.author.id, user.id)
-                    if(pastInvite == null) {
-                        val dmChannel = user
-                            .openPrivateChannel()
-                            .await()
-
-                        try {
-                            val inviteMessage = dmChannel
-                                .sendMessageEmbeds(
-                                    EmbedTemplates.normal(
-                                        "You have been invited to ${context.author.asMention} party on **${context.guild!!.name}**.",
-                                        "Party Invite"
-                                    ).build()
-                                )
-                                .setActionRow(
-                                    listOf(
-                                        Button.success("accept-party", "Accept"),
-                                        Button.danger("deny-party", "Deny")
-                                    )
-                                )
+                    if(party.players.size < 4) {
+                        val pastInvite = Bot.database.partyInviteRepository.findInvite(context.author.id, user.id)
+                        if(pastInvite == null) {
+                            val dmChannel = user
+                                .openPrivateChannel()
                                 .await()
 
-                            Bot.database.partyInviteRepository.createInvite(
-                                PartyInvite(inviteMessage.id, context.author.id, user.id)
-                            )
+                            try {
+                                val inviteMessage = dmChannel
+                                    .sendMessageEmbeds(
+                                        EmbedTemplates.normal(
+                                            "You have been invited to ${context.author.asMention} party on **${context.guild!!.name}**.",
+                                            "Party Invite"
+                                        ).build()
+                                    )
+                                    .setActionRow(
+                                        listOf(
+                                            Button.success("accept-party", "Accept"),
+                                            Button.danger("deny-party", "Deny")
+                                        )
+                                    )
+                                    .await()
 
+                                Bot.database.partyInviteRepository.createInvite(
+                                    PartyInvite(inviteMessage.id, context.author.id, user.id)
+                                )
+
+                                context.reply(
+                                    EmbedTemplates
+                                        .normal(
+                                            "${user.asMention} has been invited to your party",
+                                            "User Invited"
+                                        )
+                                        .build()
+                                ).queue()
+                            } catch (err: Throwable) {
+                                println(err)
+                                context.reply(
+                                    EmbedTemplates
+                                        .error(
+                                            "${user.asMention} has their DM's closed; you may not invite them to your party!"
+                                        )
+                                        .build()
+                                ).queue()
+                            }
+                        } else {
                             context.reply(
                                 EmbedTemplates
-                                    .normal(
-                                        "${user.asMention} has been invited to your party",
-                                        "User Invited"
-                                    )
-                                    .build()
-                            ).queue()
-                        } catch (err: Throwable) {
-                            println(err)
-                            context.reply(
-                                EmbedTemplates
-                                    .error(
-                                        "${user.asMention} has their DM's closed; you may not invite them to your party!"
-                                    )
+                                    .error("You already invited this user to a party in the past!")
                                     .build()
                             ).queue()
                         }
                     } else {
                         context.reply(
                             EmbedTemplates
-                                .error("You already invited this user to a party in the past!")
+                                .error("You cannot have more than 4 people in a party!")
                                 .build()
                         ).queue()
                     }
